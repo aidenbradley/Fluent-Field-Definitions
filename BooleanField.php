bb238b8c5f94718d85e7eafee5a596749d27989b
aidenbradley <mraidenbradley@gmail.com>
2021-11-10 20:51:31 +0000

    Adding the Boolean field type field definition

---
+++ BooleanField.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\fluent_field_definition;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\fluent_field_definitions\Base\FluentFieldDefinition;
+
+class BooleanField extends FluentFieldDefinition
+{
+    public static function fieldType(): string
+    {
+        return 'boolean';
+    }
+
+    public function onLabel(string $value): self
+    {
+        return $this->setSetting('on_label', new TranslatableMarkup($value));
+    }
+
+    public function offLabel(string $value): self
+    {
+        return $this->setSetting('off_label', new TranslatableMarkup($value));
+    }
+}
