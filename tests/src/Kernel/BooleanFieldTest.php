aa38e1843f1c6765bfd3593bd1d388ff842a2e6b
aidenbradley <mraidenbradley@gmail.com>
2021-11-28 00:48:51 +0000

    Run fieldExists assertion against the correct table based off the entity type definition

--- tests/src/Kernel/BooleanFieldTest.php
+++ tests/src/Kernel/BooleanFieldTest.php
@@ -2,24 +2,33 @@
 
 namespace Drupal\Tests\fluent_field_definitions\Kernel;
 
-use Drupal\fluent_field_definitions\BooleanField;
+use Drupal\Core\Cache\UseCacheBackendTrait;
 use Drupal\fluent_field_definitions\Base\FluentFieldDefinition;
+use Drupal\fluent_field_definitions\BooleanField;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
+use Drupal\Tests\system\Functional\Entity\Traits\EntityDefinitionTestTrait;
 
 class BooleanFieldTest extends KernelTestBase
 {
+    use EntityDefinitionTestTrait,
+        UseCacheBackendTrait;
+
     /** @var string[] */
     protected static $modules = [
         'system',
         'node',
         'user',
         'fluent_field_definitions',
+        'field',
     ];
 
     /** @var \Drupal\Core\Entity\EntityDefinitionUpdateManager */
     protected $entityDefinitionUpdateManager;
+
+    /** @var \Drupal\Core\Entity\EntityTypeManager */
+    protected $entityTypeManager;
 
     /** @var \Drupal\Core\Entity\EntityFieldManager */
     protected $entityFieldManager;
@@ -27,38 +36,48 @@ class BooleanFieldTest extends KernelTestBase
     /** @var \Drupal\Core\Database\Connection */
     protected $database;
 
-    protected function setUp()
+    protected $state;
+
+    protected function setUp(): void
     {
         parent::setUp();
-
-        $this->entityDefinitionUpdateManager = $this->container->get('entity.definition_update_manager');
-        $this->entityFieldManager = $this->container->get('entity_field.manager');
-        $this->database = $this->container->get('database');
 
         $this->installEntitySchema('node');
         $this->installEntitySchema('user');
 
+        $this->installSchema('system', 'sequences');
+
+        $this->installConfig('field');
+
+        $this->entityDefinitionUpdateManager = $this->container->get('entity.definition_update_manager');
+        $this->entityTypeManager = $this->container->get('entity_type.manager');
+        $this->entityFieldManager = $this->container->get('entity_field.manager');
+        $this->database = $this->container->get('database');
+        $this->state = $this->container->get('state');
+        $this->cacheBackend = $this->container->get('cache.discovery');
+
         NodeType::create([
-            'type' => 'page',
             'name' => 'page',
+            'type' => 'page',
         ])->save();
     }
 
     /** @test */
     public function on_label(): void
     {
-        $field = BooleanField::make()
-            ->setName('boolean_field')
+        $field = BooleanField::make('boolean_field')
             ->onLabel('We are on');
 
         $this->addFieldDefinition($field, 'node');
 
         $node = Node::create([
-            'title' => 'Boolean test',
+            'nid' => 1,
+            'title' => 'test',
             'type' => 'page',
-            'boolean_field' => 0,
+            'boolean_field' => 1,
         ]);
         $node->save();
+        $node = Node::load(1);
     }
 
     private function addFieldDefinition(FluentFieldDefinition $field, string $entityType): void
@@ -66,12 +85,14 @@ class BooleanFieldTest extends KernelTestBase
         $this->entityDefinitionUpdateManager->installFieldStorageDefinition(
             $field->getName(),
             $entityType,
-            $field->getProvider(),
-            $field
+            'fluent_field_definitions',
+            $field,
         );
 
+        $entityTypeDefinition = $this->entityTypeManager->getDefinition($entityType);
+
         $this->assertTrue(
-            $this->database->schema()->fieldExists('node', $field->getName())
+            $this->database->schema()->fieldExists($entityTypeDefinition->getDataTable(), $field->getName())
         );
     }
 }
