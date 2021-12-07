3021db80e6b29b82365003c3ed431b58428fa7a0
aidenbradley <mraidenbradley@gmail.com>
2021-12-07 17:47:07 +0000

    Adding new base field definition type for general test

---
+++ tests/src/Kernel/FluentFieldDefinitionTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\Tests\fluent_field_definitions\Kernel;
+
+use Drupal\fluent_field_definitions\StringField;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
+use Drupal\Tests\fluent_field_definitions\Kernel\Base\FluentFieldDefinitionKernelTestBase;
+
+class FluentFieldDefinitionTest extends FluentFieldDefinitionKernelTestBase
+{
+    /** @var string[] */
+    protected static $modules = [
+        'node',
+        'user',
+        'fluent_field_definitions',
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->installEntitySchema('node');
+        $this->installEntitySchema('user');
+
+        NodeType::create([
+            'name' => 'page',
+            'type' => 'page',
+        ])->save();
+    }
+
+    /** @test */
+    public function set_label()
+    {
+        $field = StringField::make('string_field')
+            ->setName('text_field')
+            ->setLabel('This is my label');
+
+        $this->installField($field);
+
+        $node = Node::create([
+            'nid' => 1,
+            'title' => 'test',
+            'type' => 'page',
+            'string_field' => 'test',
+        ]);
+        $node->save();
+        $node = Node::load(1);
+
+        $this->assertEquals(
+            'This is my label',
+            $node->get('text_field')->getFieldDefinition()->getLabel()
+        );
+    }
+}
