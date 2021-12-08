0a1ea90ced4c05292134f44afe9750a01bdbca8a
aidenbradley <mraidenbradley@gmail.com>
2021-12-08 23:03:22 +0000

    Uri field has a max length setting. Add concern to field

--- src/UriField.php
+++ src/UriField.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\fluent_field_definitions;
 
+use Drupal\fluent_field_definitions\Concerns\HasMaxLength;
+
 class UriField extends StringField
 {
+    use HasMaxLength;
+
     public static function fieldType(): string
     {
         return 'uri';
