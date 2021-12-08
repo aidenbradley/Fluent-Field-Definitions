a18c73ab1510c51a90a4da86f7d6f8cd0b0ae1c5
aidenbradley <mraidenbradley@gmail.com>
2021-12-08 01:39:37 +0000

    Adding test coverage for image field

--- src/ImageField.php
+++ src/ImageField.php
@@ -8,4 +8,34 @@ class ImageField extends FileField
     {
         return 'image';
     }
+
+    public function maximumResolution(int $width, int $height): self
+    {
+        return $this->setSetting('max_resolution', $width . 'x' . $height);
+    }
+
+    public function minimumResolution(int $width, int $height): self
+    {
+        return $this->setSetting('min_resolution', $width . 'x' . $height);
+    }
+
+    public function altFieldRequired(): self
+    {
+        return $this->setSetting('alt_field_required', true);
+    }
+
+    public function altFieldNotRequired(): self
+    {
+        return $this->setSetting('alt_field_required', false);
+    }
+
+    public function titleFieldRequired(): self
+    {
+        return $this->setSetting('title_field_required', true);
+    }
+
+    public function titleFieldNotRequired(): self
+    {
+        return $this->setSetting('title_field_required', false);
+    }
 }
---
+++ tests/src/Kernel/ImageFieldTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Drupal\Tests\fluent_field_definitions\Kernel;
+
+use Drupal\file\Entity\File;
+use Drupal\fluent_field_definitions\ImageField;
+use Drupal\node\Entity\Node;
+use Drupal\Tests\fluent_field_definitions\Kernel\Base\FluentFieldDefinitionNodeKernelTestBase;
+
+class ImageFieldTest extends FluentFieldDefinitionNodeKernelTestBase
+{
+    /** @var string[] */
+    protected static $modules = [
+        'image',
+        'file'
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->installEntitySchema('file');
+        $this->installSchema('file', 'file_usage');
+    }
+
+    /** @test */
+    public function maximum_resolution(): void
+    {
+        $field = ImageField::make('image_field')->maximumResolution(100, 100);
+
+        $this->installField($field, 'node');
+
+        $node = $this->createFileAndNode();
+
+        $this->assertEquals(
+            '100x100',
+            $node->get('image_field')->getFieldDefinition()->getSetting('max_resolution')
+        );
+    }
+
+    /** @test */
+    public function minimum_resolution(): void
+    {
+        $field = ImageField::make('image_field')->minimumResolution(100, 100);
+
+        $this->installField($field, 'node');
+
+        $node = $this->createFileAndNode();
+
+        $this->assertEquals(
+            '100x100',
+            $node->get('image_field')->getFieldDefinition()->getSetting('min_resolution')
+        );
+    }
+
+    /** @test */
+    public function alt_field_required(): void
+    {
+        $field = ImageField::make('image_field')->altFieldRequired();
+
+        $this->installField($field, 'node');
+
+        $node = $this->createFileAndNode();
+
+        $this->assertTrue(
+            $node->get('image_field')->getFieldDefinition()->getSetting('alt_field_required')
+        );
+    }
+
+    /** @test */
+    public function alt_field_not_required(): void
+    {
+        $field = ImageField::make('image_field')->altFieldNotRequired();
+
+        $this->installField($field, 'node');
+
+        $node = $this->createFileAndNode();
+
+        $this->assertFalse(
+            $node->get('image_field')->getFieldDefinition()->getSetting('alt_field_required')
+        );
+    }
+
+    /** @test */
+    public function title_field_required(): void
+    {
+        $field = ImageField::make('image_field')->titleFieldRequired();
+
+        $this->installField($field, 'node');
+
+        $node = $this->createFileAndNode();
+
+        $this->assertTrue(
+            $node->get('image_field')->getFieldDefinition()->getSetting('title_field_required')
+        );
+    }
+
+    /** @test */
+    public function title_field_not_required(): void
+    {
+        $field = ImageField::make('image_field')->titleFieldNotRequired();
+
+        $this->installField($field, 'node');
+
+        $node = $this->createFileAndNode();
+
+        $this->assertFalse(
+            $node->get('image_field')->getFieldDefinition()->getSetting('title_field_required')
+        );
+    }
+
+    private function createFileAndNode(): Node
+    {
+        File::create([
+            'fid' => 1,
+            'uri' => 'test',
+        ])->save();
+
+        return $this->createNode([
+            'image_field' => 1,
+        ]);
+    }
+}
